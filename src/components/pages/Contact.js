b51ecee3a89261599d8ba4f6e22f1d7aa5d63486
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-12 18:17:48 -0700

    Contact component code added.

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>
+                Contact info goes here.
+            </p>
+            <a href="">Gmail</a>
+            <a href="">Facebook</a>
+            <a href="">LinkedIn</a>
+        </div>
