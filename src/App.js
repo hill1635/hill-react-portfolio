c156f18c501ae9439d83135dbcf84c59f3cc3afa
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-12 18:06:32 -0700

    Changed About to Bio.  Set up React Router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Bio from ".components/pages/Bio";
+import Portfolio from ".components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Bio} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
