2d74b8a50f159ed188129f47584f601527ac1d68
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-12 18:50:04 -0700

    Footer component code added.  Imported to App.js and Fragment added to application.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Bio from "./components/pages/Bio";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Bio} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <h4>©Tucker Hill</h4>
+        </div>
+    );
+}
+
+export default Footer;
