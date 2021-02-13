41efd1f74ecb3c173188d651b9c6ebb0e17d09b5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-12 18:19:08 -0700

    Contact component formatting.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Contact() {
-    return (
-        <div>
-            <h1>Contact</h1>
-            <p>
-                Contact info goes here.
-            </p>
-            <a href="">Gmail</a>
-            <a href="">Facebook</a>
-            <a href="">LinkedIn</a>
-        </div>
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Contact info goes here.</p>
+      <a href="">Gmail</a>
+      <a href="">Facebook</a>
+      <a href="">LinkedIn</a>
+    </div>
+  );
+}
+
+export default Contact;
