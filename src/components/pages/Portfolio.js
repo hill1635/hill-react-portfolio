4a0e6df4bb1f696fe85dbda8bca0682d040faf36
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-15 23:33:27 -0700

    Dividers added between headers and content on pages.  CSS added to center headers and divider width to 50%.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,7 @@ function Portfolio() {
     return (
         <div>
             <h1>Portfolio</h1>
+            <hr className="solid"></hr>
             <Project />
             <p>Check out my GitHub page <a href="https://github.com/hill1635">here</a>.</p>
         </div>
