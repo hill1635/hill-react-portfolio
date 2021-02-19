4174e12bf2831784fb3cab3be32d17acb48d78a0
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-18 22:51:38 -0700

    Navbar code added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,25 +5,37 @@ function Navbar() {
   const location = useLocation;
 
   return (
-    <header>
-      <ul>
-        <li>
-          <Link to="/" className="breadcrumb-divider">
-            Bio
-          </Link>
-        </li>
-        <li>
-          <Link to="/portfolio" className="breadcrumb-divider">
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="breadcrumb-divider">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand disabled" href="#">
+          Tucker Hill
+        </span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link to="/" className="nav-link">
+              Bio
+            </Link>
+            <Link to="/portfolio" className="nav-link">
+              Portfolio
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 
