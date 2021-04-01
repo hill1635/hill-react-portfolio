331daee04469cf48af887beb5b1b53de06865757
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-01 16:30:57 -0600

    Icons aded to public icons folder.  Icons imported into Portfolio page.  CSS file added for Portfolio page with icon styling.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Project from "../Project";
+import "./Portfolio.css";
 
 function Portfolio() {
-    return (
-        <main className="portfolio">
-            <h1>Portfolio</h1>
-            <hr className="solid"></hr>
-            <Project />
-        </main>
-    );
+  return (
+    <main className="portfolio">
+      <h1>Portfolio</h1>
+      <hr className="solid"></hr>
+      <div className="icons">
+        <img src="./icons/star.svg"></img>
+        <img src="./icons/bootstrap.svg"></img>
+        <img src="./icons/javascript.svg"></img>
+        <img src="./icons/jquery.svg"></img>
+        <img src="./icons/nodejs.svg"></img>
+        <img src="./icons/react.svg"></img>
+        <img src="./icons/mongodb.svg"></img>
+        <img src="./icons/mysql.svg"></img>
+      </div>
+      <div>
+        <Project />
+      </div>
+    </main>
+  );
 }
 
 export default Portfolio;
