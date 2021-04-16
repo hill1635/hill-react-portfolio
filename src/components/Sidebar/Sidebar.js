139475b3999e37427734ad7c308b1ddda6c9054e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-16 16:54:47 -0600

    Button styling.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,14 +4,14 @@ import "./Sidebar.css";
 function Sidebar() {
   return (
     <div className="btn-group-vertical buttons position-fixed">
-      <button className="rounded-circle mb-2"><img src="./icons/star.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/bootstrap.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/javascript.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/jquery.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/nodejs.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/react.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/mongodb.svg"></img></button>
-      <button className="rounded-circle mb-2"><img src="./icons/mysql.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/star.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/bootstrap.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/javascript.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/jquery.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/nodejs.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/react.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/mongodb.svg"></img></button>
+      <button className="rounded-circle mb-2 btn-dark"><img src="./icons/mysql.svg"></img></button>
     </div>
   );
 }
