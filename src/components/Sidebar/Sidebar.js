c68276a8cd0b5af1c8144ad971ab9e516b722a01
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-16 16:52:57 -0600

    Sidebar styling.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,15 +3,15 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="icons">
-      <img src="./icons/star.svg"></img>
-      <img src="./icons/bootstrap.svg"></img>
-      <img src="./icons/javascript.svg"></img>
-      <img src="./icons/jquery.svg"></img>
-      <img src="./icons/nodejs.svg"></img>
-      <img src="./icons/react.svg"></img>
-      <img src="./icons/mongodb.svg"></img>
-      <img src="./icons/mysql.svg"></img>
+    <div className="btn-group-vertical buttons position-fixed">
+      <button className="rounded-circle mb-2"><img src="./icons/star.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/bootstrap.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/javascript.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/jquery.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/nodejs.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/react.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/mongodb.svg"></img></button>
+      <button className="rounded-circle mb-2"><img src="./icons/mysql.svg"></img></button>
     </div>
   );
 }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -5,16 +5,18 @@ import Sidebar from "../../components/Sidebar/Sidebar.js";
 
 function Portfolio() {
   return (
-    <main className="portfolio">
-      <h1>Portfolio</h1>
-      <hr className="solid"></hr>
-      <div>
+    <div>
+      <aside className="float-left">
         <Sidebar />
-      </div>
-      <div>
-        <Project />
-      </div>
-    </main>
+      </aside>
+      <main className="portfolio">
+        <h1>Portfolio</h1>
+        <hr className="solid"></hr>
+        <div>
+          <Project />
+        </div>
+      </main>
+    </div>
   );
 }
 
