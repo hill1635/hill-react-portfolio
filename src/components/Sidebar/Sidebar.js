26a17d064bd0c78a6faeac473de0c370d12cfb7b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-16 16:25:28 -0600

    Sidebar component created.  Icons moved from Portfolio to Sidebar.

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="icons">
+      <img src="./icons/star.svg"></img>
+      <img src="./icons/bootstrap.svg"></img>
+      <img src="./icons/javascript.svg"></img>
+      <img src="./icons/jquery.svg"></img>
+      <img src="./icons/nodejs.svg"></img>
+      <img src="./icons/react.svg"></img>
+      <img src="./icons/mongodb.svg"></img>
+      <img src="./icons/mysql.svg"></img>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,21 +1,15 @@
 import React from "react";
 import Project from "../../components/Project/Project.js";
 import "./Portfolio.css";
+import Sidebar from "../../components/Sidebar/Sidebar.js";
 
 function Portfolio() {
   return (
     <main className="portfolio">
       <h1>Portfolio</h1>
       <hr className="solid"></hr>
-      <div className="icons">
-        <img src="./icons/star.svg"></img>
-        <img src="./icons/bootstrap.svg"></img>
-        <img src="./icons/javascript.svg"></img>
-        <img src="./icons/jquery.svg"></img>
-        <img src="./icons/nodejs.svg"></img>
-        <img src="./icons/react.svg"></img>
-        <img src="./icons/mongodb.svg"></img>
-        <img src="./icons/mysql.svg"></img>
+      <div>
+        <Sidebar />
       </div>
       <div>
         <Project />
