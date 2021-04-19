b687ba328d72f0a400a789cc5cdfb1e5f6dd98f9
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-19 11:59:29 -0600

    Deleted CDN, added Vanilla JS typing effect for Bio header.  Still some tweaking left to do though.

--- src/pages/Bio/Bio.js
+++ src/pages/Bio/Bio.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Bio.css";
+import Typed from "react-typed";
 
 function Bio() {
   return (
     <main className="bio">
-      <h1>About Me</h1>
+      <h1 className="header">About Me.</h1>
       <hr className="solid"></hr>
       <p>Hello World!  I am a Full-Stack Developer with five months of experience.  I am currently enrolled in the University of Utah Coding Bootcamp and on track to graduate in March.  My front-end wheelhouse is in HTML, CSS, and Javascript.  My CSS background expands to the Bootstrap framework.  Javscript libraries learned include jQuery and React as well as server-side technologies such as AJAX and APIs.  On the back-end, I am knowledgeable in the Node.js environment and server creation with Node, Express and HTTP.  Database construction with MySQL, MongoDB, and IndexedDB using ORM and MVC techniques with Sequelize and Mongoose.  Sprinkle on top of that some testing with Jest and Progressive Web Apps with Webpack and that sums up the past five months for me!</p>
       <p>My goal as a Developer is to continue honing my current skills and becoming one step closer to mastery.  In addition to this, I strive to continue learning new skills, technologies and languages.  Coding is a continually evolving craft and the sheer amount of tools out there to learn is incredibly exciting as one who is always looking to push myself further and grow.  Flexibility is also key to providing clients with the best options suited to their specific needs.</p>
