ebcb9629bc0afeaa3a197dce2f41a14f56fc8361
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-20 14:05:22 -0600

    Footer styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import "./Footer.css";
 function Footer() {
   return (
       <footer className="footer col-12 p-3">
+        <p className="copywrite mt-2 mb-0">©Tucker Hill</p>
         <a target="_blank" href="https://www.github.com/hill1635"><img src="./icons/github.svg" className="icon"></img></a>
         <a target="_blank" href="https://www.facebook.com/tucker.hill.75/"><img src="./icons/facebook.svg" className="icon-fb"></img></a>
         <a target="_blank" href="https://www.linkedin.com/in/tucker-hill-5254b979/"><img src="./icons/linkedin.svg" className="icon"></img></a>
         <a target="_blank" href="mailto:hill1635@gmail.com"><img src="./icons/gmail.svg" className="icon"></img></a>
-        <p className="copywrite mt-2 mb-0">©Tucker Hill</p>
       </footer>
   );
 }
