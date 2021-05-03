2c9874ba16a7aa50b88c6aacb39573dd4bf835d2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-05-03 15:29:48 -0600

    Gradient borders added to Nav and Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,8 @@ import "./Footer.css";
 
 function Footer() {
   return (
+    <div className="wrapper">
+      <div className="footer-border"></div>
       <footer className="footer col-12 p-3">
         <p className="copywrite mt-2 mb-0">©Tucker Hill</p>
         <a target="_blank" href="https://www.github.com/hill1635"><img src="./icons/github.svg" className="icon"></img></a>
@@ -10,6 +12,7 @@ function Footer() {
         <a target="_blank" href="https://www.linkedin.com/in/tucker-hill-5254b979/"><img src="./icons/linkedin.svg" className="icon"></img></a>
         <a target="_blank" href="mailto:hill1635@gmail.com"><img src="./icons/gmail.svg" className="icon"></img></a>
       </footer>
+      </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
 
   window.addEventListener("scroll", () => {
     const currentPosition = window.pageYOffset;
-    if(currentPosition <= scrollLength) {
+    if (currentPosition <= scrollLength) {
       opacity = 1 - currentPosition / scrollLength;
     } else {
       opacity = 0;
@@ -17,37 +17,38 @@ function Navbar() {
   });
 
   return (
-    <nav className="navbar navbar-expand-lg col-12 shadow-lg">
-      <div className="container container-fluid col-12">
-        <span className="name mr-3">
-          Tucker Hill
-        </span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="links navbar-nav">
-            <Link to="/" className="nav-link text-white">
-              Bio
-            </Link>
-            <Link to="/portfolio" className="nav-link text-white">
-              Portfolio
-            </Link>
-            <Link to="/contact" className="nav-link text-white">
-              Contact
-            </Link>
+    <div className="wrapper">
+      <nav className="navbar navbar-expand-lg col-12 shadow-lg">
+        <div className="container container-fluid col-12">
+          <span className="name mr-3">Tucker Hill</span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="links navbar-nav">
+              <Link to="/" className="nav-link text-white">
+                Bio
+              </Link>
+              <Link to="/portfolio" className="nav-link text-white">
+                Portfolio
+              </Link>
+              <Link to="/contact" className="nav-link text-white">
+                Contact
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div className="nav-border"></div>
+    </div>
   );
 }
 
