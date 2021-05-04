fdf1a76f6c0ea3e7d0c14b7c055b83e3273efde0
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-05-04 12:03:57 -0600

    Project card styling.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,13 +6,15 @@ function Project() {
   return (
     <div className="projects col-12">
       {projects.map((project) => (
-        <div className="bg-transparent project-card col-lg-5 col-sm-12 mx-auto mb-5 shadow-sm p-1">
-            <img src={project.img} alt={project.title} className="project-img m-0 p-0 border-bottom"></img>
-          <div className="card-body bg-white project-body text-center p-3">
-            <h4>{project.title}</h4>
-            <a href={project.github} target="_blank">GitHub</a>
-            <i class="fas fa-grip-lines-vertical mx-1"></i>
-            <a href={project.deployed} target="_blank">Deployed</a>
+        <div className="project-card col-lg-5 col-sm-12 mx-auto mb-5 shadow-sm px-0">
+          <div className="card col-12 border-0 bg-transparent px-0">
+              <img src={project.img} alt={project.title} className="project-img m-0 p-0 border-bottom"></img>
+            <div className="card-body bg-white project-body text-center px-0">
+              <h4>{project.title}</h4>
+              <a href={project.github} target="_blank">GitHub</a>
+              <i class="fas fa-grip-lines-vertical mx-1"></i>
+              <a href={project.deployed} target="_blank">Deployed</a>
+            </div>
           </div>
         </div>
       ))}
