12829c0fb001d1d0def5028630635e20c09728a1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-05-03 18:54:33 -0600

    Footer styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,9 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="wrapper">
-      <div className="footer-border"></div>
-      <footer className="footer col-12 pt-2 pb-3">
+    <div className="wrapper p-3">
+      <div className="footer-border rounded-top"></div>
+      <footer className="footer col-12 pt-2 pb-3 rounded-bottom">
         <p className="copywrite mt-2 mb-0">©Tucker Hill</p>
         <a target="_blank" href="https://www.github.com/hill1635"><img src="./icons/github.svg" className="icon"></img></a>
         <a target="_blank" href="https://www.facebook.com/tucker.hill.75/"><img src="./icons/facebook.svg" className="icon-fb"></img></a>
