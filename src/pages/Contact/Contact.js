a0c2564583fac3389a3372a7f0dd1ca3125488df
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-05-07 01:11:54 -0600

    Contact page styling.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,20 +3,26 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <main className="contact col-6 mt-5 pt-2">
-      <h1 className="header">Contact.</h1>
-      <hr className="solid"></hr>
-      <div className="content">
-        <p>Phone: (207) 890-8694</p>
-        <p>
-          Email:
-          <a href="mailto:hill1635@gmail.com" target="_blank">
-            hill1635@gmail.com
-          </a>
-        </p>
-        <a href="https://www.linkedin.com/in/tucker-hill-5254b979/" target="_blank">
-          LinkedIn
-        </a>
+    <main className="stack col-7 bg-transparent p-0">
+      <div className="card-2 bg-white">
+        <div className="card-1 bg-white">
+          <div className="contact bg-white p-5">
+            <h1 className="header">Contact.</h1>
+            <hr className="solid"></hr>
+            <div className="content">
+              <p>Phone: (207) 890-8694</p>
+              <p>
+                Email:
+                <a href="mailto:hill1635@gmail.com" target="_blank">
+                  hill1635@gmail.com
+                </a>
+              </p>
+              <a href="https://www.linkedin.com/in/tucker-hill-5254b979/" target="_blank">
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
