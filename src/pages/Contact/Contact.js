06ea41c6f0b10b112b48d75eeaac15217c73006d
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-05-12 13:52:03 -0600

    Font and contact card styling.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,22 +4,36 @@ import "./Contact.css";
 function Contact() {
   return (
     <main className="stack col-7 bg-transparent p-0">
-      <div className="card-2 bg-white">
-        <div className="card-1 bg-white">
-          <div className="contact bg-white p-5">
-            <h1 className="header">Contact.</h1>
-            <hr className="solid"></hr>
-            <div className="content">
-              <p>Phone: (207) 890-8694</p>
-              <p>
-                Email:
-                <a href="mailto:hill1635@gmail.com" target="_blank">
-                  hill1635@gmail.com
-                </a>
-              </p>
-              <a href="https://www.linkedin.com/in/tucker-hill-5254b979/" target="_blank">
-                LinkedIn
-              </a>
+      <div className="card-1 row">
+        <div className="card-2 row bg-white">
+          <div className="card-3 row bg-white">
+            <div className="contact row bg-white">
+              <div className="logo col-4">
+                <div className="letters">
+                  <span className="col-12 letter-t">T</span>
+                  <span className="col-12 letter-h">H</span>
+                </div>
+              </div>
+              <div className="space col-1"></div>
+              <div className="info col-7 p-5">
+                <h1 className="card-name">Tucker Hill</h1>
+                <hr className="solid bg-dark"></hr>
+                <div className="content mt-5">
+                  <p>(207) 890-8694
+                  <i class="fas fa-phone-square p-1"></i>
+                  </p>
+                  <p>
+                    <a href="mailto:hill1635@gmail.com" target="_blank">
+                      hill1635@gmail.com
+                    </a>
+                    <i class="fas fa-envelope-square"></i>
+                  </p>
+                  <a href="https://www.linkedin.com/in/tucker-hill-5254b979/" target="_blank">
+                    LinkedIn
+                  </a>
+                  <i class="fab fa-linkedin"></i>
+                </div>
+              </div>
             </div>
           </div>
         </div>
