4f288ede544aec1b990338461a81faab0369695b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-10-25 23:35:06 -0600

    Project card styling.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,12 +4,12 @@ import projects from "../../projects.json";
 
 function Project() {
   return (
-    <div className="projects col-12 p-0">
+    <div className="projects col-12 p-0 rd-15">
       {projects.map((project) => (
-        <div className="project-card mx-auto mb-5 px-0 rounded">
-          <div className="col-12 border-0 bg-transparent px-0 rounded-top">
-            <img src={project.img} alt={project.title} className="project-img m-0 p-0 rounded-top"></img>
-            <div className="card-body bg-white project-body text-center px-0 rounded-bottom">
+        <div className="project-card mx-auto mb-5 px-0 rd-15 shadow">
+          <div className="col-12 border-0 bg-transparent px-0 rd-15">
+            <img src={project.img} alt={project.title} className="project-img m-0 p-0 rd-15-top"></img>
+            <div className="card-body bg-white project-body text-center px-0 rd-15-bottom">
               <h4>{project.title}</h4>
               <a href={project.github} target="_blank">GitHub</a>
               <i class="fas fa-grip-lines-vertical mx-1"></i>
