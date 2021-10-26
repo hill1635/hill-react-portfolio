1dfe05939857cfc0869589e7210dcb15064e54f2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-10-26 13:08:16 -0600

    More navbar styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -64,21 +64,21 @@ function Navbar() {
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="links navbar-nav mx-auto">
-            <Link to="/" className="nav-link mr-5 mr-md-4">
+            <Link to="/" className="nav-link mr-lg-5 mr-md-4">
               <i className="fas fa-angle-right mr-1 b-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">About</span>
                 <div className="underline u-about"></div>
               </div>
             </Link>
-            <Link to="/portfolio" className="nav-link mx-5 mx-md-4">
+            <Link to="/portfolio" className="nav-link mx-lg-5 mx-md-4">
               <i className="fas fa-angle-right mr-1 p-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Portfolio</span>
                 <div className="underline u-portfolio"></div>
               </div>
             </Link>
-            <Link to="/contact" className="nav-link ml-5 ml-md-4">
+            <Link to="/contact" className="nav-link ml-lg-5 ml-md-4">
               <i className="fas fa-angle-right mr-1 c-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Contact</span>
