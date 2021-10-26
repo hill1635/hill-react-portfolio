9ddc30ef44f1853322aff9040d1cb895aeb90b97
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-10-26 12:27:53 -0600

    Links added to navbar buttons.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,14 +17,15 @@ function Navbar() {
   });
 
   return (
-      <nav className="navbar navbar-expand-lg col-12 pt-4 pb-2 mb-5">
-        <span className="name position-fixed shadow-sm mt-2">
-          <span className="col-12 initial-t">T</span>
-          <span className="col-12 initial-h">H</span>
-        </span>
-        <div className="container container-fluid col-12">
+    <nav className="navbar navbar-expand-lg col-12 pt-4 pb-2 mb-5">
+      <span className="name position-fixed shadow-sm mt-2">
+        <span className="col-12 initial-t">T</span>
+        <span className="col-12 initial-h">H</span>
+      </span>
+      <div className="container container-fluid col-5">
+        <Link to="/">
           <button
-            className="navbar-toggler border border-solid rounded"
+            className="navbar-toggler menu-btn rounded-circle homeBtn"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavAltMarkup"
@@ -32,35 +33,62 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i class="fas fa-align-right menu-btn"></i>
+            <i class="fas fa-home"></i>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="links navbar-nav mx-auto">
-              <Link to="/" className="nav-link mr-5">
+        </Link>
+        <Link to="/portfolio">
+          <button
+            className="navbar-toggler rounded-circle menu-btn portfolioBtn"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-book-open"></i>
+          </button>
+        </Link>
+        <Link to="/contact">
+          <button
+            className="navbar-toggler rounded-circle menu-btn contactBtn text-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-info-circle"></i>
+          </button>
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="links navbar-nav mx-auto">
+            <Link to="/" className="nav-link mr-5">
               <i className="fas fa-angle-right mr-1 b-icon"></i>
               <div className="d-inline-block">
-              <span className="link-text">About</span>
-              <div className="underline u-about"></div>
+                <span className="link-text">About</span>
+                <div className="underline u-about"></div>
               </div>
-              </Link>
-              <Link to="/portfolio" className="nav-link mx-5">
+            </Link>
+            <Link to="/portfolio" className="nav-link mx-5">
               <i className="fas fa-angle-right mr-1 p-icon"></i>
               <div className="d-inline-block">
-              <span className="link-text">Portfolio</span>
-              <div className="underline u-portfolio"></div>
+                <span className="link-text">Portfolio</span>
+                <div className="underline u-portfolio"></div>
               </div>
-              </Link>
-              <Link to="/contact" className="nav-link ml-5">
-                <i className="fas fa-angle-right mr-1 c-icon"></i>
-                <div className="d-inline-block">
+            </Link>
+            <Link to="/contact" className="nav-link ml-5">
+              <i className="fas fa-angle-right mr-1 c-icon"></i>
+              <div className="d-inline-block">
                 <span className="link-text">Contact</span>
                 <div className="underline u-contact"></div>
-                </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
 
