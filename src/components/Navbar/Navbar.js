837be8a45fa941f7525ab415958e2e2d6210af6e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-10-26 13:01:10 -0600

    Navbar styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,12 +17,12 @@ function Navbar() {
   });
 
   return (
-    <nav className="navbar navbar-expand-lg col-12 pt-4 pb-2 mb-5">
+    <nav className="navbar navbar-expand-md col-12 pt-4 pb-2 mb-5">
       <span className="name position-fixed shadow-sm mt-2">
         <span className="col-12 initial-t">T</span>
         <span className="col-12 initial-h">H</span>
       </span>
-      <div className="container container-fluid col-5">
+      <div className="container container-fluid col-6">
         <Link to="/">
           <button
             className="navbar-toggler menu-btn rounded-circle homeBtn"
@@ -64,21 +64,21 @@ function Navbar() {
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="links navbar-nav mx-auto">
-            <Link to="/" className="nav-link mr-5">
+            <Link to="/" className="nav-link mr-5 mr-md-4">
               <i className="fas fa-angle-right mr-1 b-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">About</span>
                 <div className="underline u-about"></div>
               </div>
             </Link>
-            <Link to="/portfolio" className="nav-link mx-5">
+            <Link to="/portfolio" className="nav-link mx-5 mx-md-4">
               <i className="fas fa-angle-right mr-1 p-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Portfolio</span>
                 <div className="underline u-portfolio"></div>
               </div>
             </Link>
-            <Link to="/contact" className="nav-link ml-5">
+            <Link to="/contact" className="nav-link ml-5 ml-md-4">
               <i className="fas fa-angle-right mr-1 c-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Contact</span>
