0c80775b2b61c892ce0707f77e487c432a22a3d5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-02 14:20:21 -0600

    Navbar link styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -51,22 +51,22 @@ function Navbar() {
           </button>
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="links navbar-nav mx-auto">
-            <Link to="/" className="nav-link mr-lg-5 mr-md-4">
+          <div class="navbar-nav mx-auto">
+            <Link to="/" className="nav-link links mr-lg-5 mr-md-4 py-2 px-3 shadow">
               <i className="fas fa-angle-right mr-1 b-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">About</span>
                 <div className="underline u-about"></div>
               </div>
             </Link>
-            <Link to="/portfolio" className="nav-link mx-lg-5 mx-md-4">
+            <Link to="/portfolio" className="nav-link links mx-lg-5 mx-md-4 py-2 px-3 shadow">
               <i className="fas fa-angle-right mr-1 p-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Portfolio</span>
                 <div className="underline u-portfolio"></div>
               </div>
             </Link>
-            <Link to="/contact" className="nav-link ml-lg-5 ml-md-4">
+            <Link to="/contact" className="nav-link links ml-lg-5 ml-md-4 py-2 px-3 shadow">
               <i className="fas fa-angle-right mr-1 c-icon"></i>
               <div className="d-inline-block">
                 <span className="link-text">Contact</span>
