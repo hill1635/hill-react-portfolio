bb057c0e1f72b093aac8e9dd4285106a894ca66b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-09 19:34:49 -0700

    Home page linked in App.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.js";
 import Footer from "./components/Footer/Footer.js";
+import Home from "./pages/Home/Home.js";
 import Bio from "./pages/Bio/Bio.js";
 import Portfolio from "./pages/Portfolio/Portfolio.js";
 import Contact from "./pages/Contact/Contact.js";
@@ -15,7 +16,7 @@ function App() {
     <Router>
       <div className="container-flex col-12 p-0 m-0">
         <Navbar />
-        <Route exact path="/" component={Bio} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
         <Footer />
