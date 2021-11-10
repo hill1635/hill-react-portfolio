c7ec1ef9bea689fac13f95e69b0a7958d933cbe1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-09 18:44:04 -0700

    Footer styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,24 +3,20 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="wrapper p-3 col-12">
-      <div className="footer-border rounded-top"></div>
-      <footer className="footer col-12 pt-1 pb-3 rounded-bottom">
-        <p className="copywrite mt-2 mb-0">©Tucker Hill</p>
+      <footer className="col-12">
         <a target="_blank" href="https://www.github.com/hill1635">
-          <img src="./icons/github.svg" className="icon"></img>
+          <img src="./icons/github.svg" className="icon rounded"></img>
         </a>
         <a target="_blank" href="https://www.facebook.com/tucker.hill.75/">
-          <img src="./icons/facebook.svg" className="icon-fb"></img>
+          <img src="./icons/facebook.svg" className="icon-fb rounded"></img>
         </a>
         <a target="_blank" href="https://www.linkedin.com/in/tucker-hill-5254b979/">
-          <img src="./icons/linkedin.svg" className="icon"></img>
+          <img src="./icons/linkedin.svg" className="icon rounded"></img>
         </a>
         <a target="_blank" href="mailto:hill1635@gmail.com">
-          <img src="./icons/gmail.svg" className="icon"></img>
+          <img src="./icons/gmail.svg" className="icon rounded"></img>
         </a>
       </footer>
-    </div>
   );
 }
 
