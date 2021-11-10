f43a91b0c047233385a99103b5100936cd4d3ea1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-09 19:56:52 -0700

    Updated links in navbar, added home link to name brand.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,13 +5,17 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-md col-12 pt-4">
-      <span className="name shadow-sm mt-3 py-2 px-3 shadow-lg rounded position-absolute">
-        <span className="first col-12 d-block text-center m-0 p-0">TUCKER</span>
-        <hr className="col-7 my-0 mx-auto divider"></hr>
-        <span className="last col-12 d-block text-center m-0 p-0">HILL</span>
-      </span>
+      <Link to="/">
+        <span className="name shadow-sm mt-3 py-2 px-3 shadow-lg rounded position-absolute">
+          <span className="first col-12 d-block text-center m-0 p-0">
+            TUCKER
+          </span>
+          <hr className="col-7 my-0 mx-auto divider"></hr>
+          <span className="last col-12 d-block text-center m-0 p-0">HILL</span>
+        </span>
+      </Link>
       <div className="container container-fluid col-6">
-        <Link to="/">
+        <Link to="/about">
           <button
             className="navbar-toggler menu-btn rounded-circle homeBtn"
             type="button"
@@ -52,19 +56,28 @@ function Navbar() {
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav mx-auto">
-            <Link to="/" className="nav-link links mr-lg-5 mr-md-4 py-2 px-3 bg-light shadow rounded">
+            <Link
+              to="/about"
+              className="nav-link links mr-lg-5 mr-md-4 py-2 px-3 bg-light shadow rounded"
+            >
               <div className="d-inline-block">
                 <span className="link-text">About</span>
                 <div className="underline u-about"></div>
               </div>
             </Link>
-            <Link to="/portfolio" className="nav-link links mx-lg-5 mx-md-4 py-2 px-3 bg-light shadow rounded">
+            <Link
+              to="/portfolio"
+              className="nav-link links mx-lg-5 mx-md-4 py-2 px-3 bg-light shadow rounded"
+            >
               <div className="d-inline-block">
                 <span className="link-text">Portfolio</span>
                 <div className="underline u-portfolio"></div>
               </div>
             </Link>
-            <Link to="/contact" className="nav-link links ml-lg-5 ml-md-4 py-2 px-3 bg-light shadow rounded">
+            <Link
+              to="/contact"
+              className="nav-link links ml-lg-5 ml-md-4 py-2 px-3 bg-light shadow rounded"
+            >
               <div className="d-inline-block">
                 <span className="link-text">Contact</span>
                 <div className="underline u-contact"></div>
