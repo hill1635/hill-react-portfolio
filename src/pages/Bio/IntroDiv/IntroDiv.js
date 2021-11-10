b0773b4992e9a0b5112f53b57935a10323ac8327
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-09 18:12:30 -0700

    Styling.

--- src/pages/Bio/IntroDiv/IntroDiv.js
+++ src/pages/Bio/IntroDiv/IntroDiv.js
@@ -4,27 +4,27 @@ import "./IntroDiv.css";
 function IntroDiv() {
   return (
     <div className="row col-xs-12 col-md-10 col-lg-8 mt-5 mx-auto">
-      <div className="bg-intro col-12 mx-auto shadow-lg p-0">
+      <div className="bg-intro col-12 mx-auto shadow-lg p-0 rounded">
         <div className="text-center">
-          <div className="hello bg-light p-3 mb-5 shadow-lg">
+          <div className="hello bg-light p-3 mb-5 shadow-lg rounded">
             <h1 className="helloText bg-light">'Hello World!'</h1>
           </div>
           <div className="clearfix">
-            <div className="biopic bg-light d-inline-block col-6 float-left p-0">
+            <div className="biopic fadeIn bg-light d-inline-block col-6 float-left p-0">
               <img
                 src="./images/me.jpeg"
                 className="rounded-circle shadow-lg m-0 w-100"
               ></img>
             </div>
-            <div className="intro bg-light d-inline-block shadow-lg col-6 float-right mt-5">
+            <div className="intro fadeIn bg-light d-inline-block shadow-lg col-6 float-right mt-5 rounded">
               <p className="py-5 px-4">
                 My name is Tucker and I'm a full-stack developer.
               </p>
             </div>
           </div>
-          <div className="learnMore bg-light w-25">
-            <span className="next d-block">
-              <p className="intro">Learn more.</p>
+          <div className="learnMore intro fadeIn bg-light w-25 p-1 rounded">
+            <span className="next">
+              <p className="mx-auto">Learn more.</p>
               <i class="fas fa-chevron-down arrow"></i>
               <i class="fas fa-chevron-down arrow"></i>
               <i class="fas fa-chevron-down arrow"></i>
