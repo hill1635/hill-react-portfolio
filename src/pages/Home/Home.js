c3a72a1b21bf3cb1439be7af3a428318fa31e054
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-11-09 19:33:04 -0700

    Home page created.  Separating IntroDiv with AboutMeDiv into separate pages.

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <main className="row col-xs-12 col-md-10 col-lg-8 mt-5 mx-auto">
+      <div className="bg-intro col-12 mx-auto shadow-lg p-0 rounded">
+        <div className="text-center">
+          <div className="hello bg-light p-3 mb-5 shadow-lg rounded">
+            <h1 className="helloText bg-light">'Hello World!'</h1>
+          </div>
+          <div className="clearfix">
+            <div className="biopic fadeIn bg-light d-inline-block col-6 float-left p-0">
+              <img
+                src="./images/me.jpeg"
+                className="rounded-circle shadow-lg m-0 w-100"
+              ></img>
+            </div>
+            <div className="intro fadeIn bg-light d-inline-block shadow-lg col-6 float-right mt-5 rounded">
+              <p className="py-5 px-4">
+                My name is Tucker and I'm a full-stack developer.
+              </p>
+            </div>
+          </div>
+          <div className="learnMore intro fadeIn bg-light w-25 p-1 rounded">
+            <span className="next">
+              <p className="mx-auto">Learn more.</p>
+              <i class="fas fa-chevron-down arrow"></i>
+              <i class="fas fa-chevron-down arrow"></i>
+              <i class="fas fa-chevron-down arrow"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
