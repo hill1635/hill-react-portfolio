60f2dbf4373edb1bab8cdd64f39ee7d8c06eab51
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-11 10:31:15 -0700

    Rearranged footer icons.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,20 +3,23 @@ import "./Footer.css";
 
 function Footer() {
   return (
-      <footer className="col-12">
-        <a target="_blank" href="https://www.github.com/hill1635">
-          <img src="./icons/github.svg" className="icon rounded"></img>
-        </a>
-        <a target="_blank" href="https://www.facebook.com/tucker.hill.75/">
-          <img src="./icons/facebook.svg" className="icon-fb rounded"></img>
-        </a>
-        <a target="_blank" href="https://www.linkedin.com/in/tucker-hill-5254b979/">
-          <img src="./icons/linkedin.svg" className="icon rounded"></img>
-        </a>
-        <a target="_blank" href="mailto:hill1635@gmail.com">
-          <img src="./icons/gmail.svg" className="icon rounded"></img>
-        </a>
-      </footer>
+    <footer className="col-12">
+      <a target="_blank" href="https://www.facebook.com/tucker.hill.75/">
+        <img src="./icons/facebook.svg" className="icon-fb rounded"></img>
+      </a>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/tucker-hill-5254b979/"
+      >
+        <img src="./icons/linkedin.svg" className="icon rounded"></img>
+      </a>
+      <a target="_blank" href="mailto:hill1635@gmail.com">
+        <img src="./icons/gmail.svg" className="icon rounded"></img>
+      </a>
+      <a target="_blank" href="https://www.github.com/hill1635">
+        <img src="./icons/github.svg" className="icon rounded"></img>
+      </a>
+    </footer>
   );
 }
 
