0252bf68756b3879e3b3e52d5538de71679754ae
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-11 11:40:36 -0700

    Added typed name header to About page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -16,6 +16,9 @@ function About() {
   return (
     <main className="aboutMe col-xs-12 col-md-10 col-lg-8 mt-5 mx-auto">
       <div className="bg-about row col-12 mx-auto shadow-lg p-0 rounded">
+        <div className="name bg-light p-3 mb-5 shadow-lg rounded mx-auto">
+          <h1 className="helloText bg-light">Tucker Hill</h1>
+        </div>
       </div>
     </main>
   );
