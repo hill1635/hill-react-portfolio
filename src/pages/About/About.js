bb45d4afeca87674e4c5b6758bc60ad7e990995b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 12:02:37 -0700

    Deleted unused useEffect and import.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,18 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./About.css";
 
 function About() {
-  // useEffect(() => {
-  //   var next = document.querySelector(".next");
-  //   var aboutMe = document.querySelector(".aboutMe");
-
-  //   next.addEventListener("click", function () {
-  //     aboutMe.style.display = "block";
-  //     window.scroll({ top: 765, behavior: "smooth" });
-  //   });
-
-  // });
-
   return (
     <main className="col-xs-12 col-md-10 col-lg-8 mt-5 mx-auto">
       <div className="bg-about row col-12 mx-auto shadow-lg p-0 rounded">
