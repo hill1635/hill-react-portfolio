bf84a3126a03b07a6b4b19dbcf708224f8f717cb
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 16:24:40 -0700

    Background info transferred over to component.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BasicInfo from "./BasicInfo";
 import FunFacts from "./FunFacts";
 import "./style.css";
 
@@ -9,21 +10,7 @@ function About() {
         <div className="header bg-light p-3 mb-5 shadow-lg rounded mx-auto">
           <h1 className="typedText bg-light">Tucker Hill</h1>
         </div>
-        <section className="col-11 clearfix mx-auto my-4 p-0">
-          <div className="picDiv col-7 col-md-6 col-lg-5 float-right p-0 rounded">
-            <img src="./images/me.jpeg" className="profilePic float-right p-4 rounded shadow m-0 w-100 bg-light"></img>
-          </div>
-          <div className="text-wrap bg-light rounded p-4">
-            <p><strong>From: </strong> Norway, ME</p>
-            <p><strong>Current: </strong> Cottonwood Heights, UT</p>
-            <p><strong>Nickname: </strong> Tuck</p>
-            <p><strong>Aliases: </strong>Trevor, Tyler, Hunter, Trucker (it's easy to mix up my name, apparently)</p>
-            <p><strong>Hobbies: </strong>Hiking, Skiing, Traveling, Playing Guitar, Video Games, Cooking (when I feel like it)</p>
-            <p><strong>Education: </strong> B.A. in Psychology and Sociology, University of Vermont</p>
-            <p><strong>Certifications: </strong>Full-Stack Development, University of Utah Coding Bootcamp</p>
-            <p><strong>Languages: </strong>JavaScript, HTML, French (un petit peu), Italian (two semesters in college), Sarcasm</p>
-          </div>
-        </section>
+        <BasicInfo/>
         <section className="col-11 bg-light rounded shadow-lg mx-auto my-4 p-4">
           <p><strong>Background: </strong> MERN stack (mostly)</p>
           {/* Carousel? */}
