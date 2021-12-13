b75c66dea29e282e0eacae21befeae0014c3a6e5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 12:10:54 -0700

    Deleted ul, converted li elemennts to div elements for carousel.

--- src/pages/About/FunFacts/FunFacts.js
+++ src/pages/About/FunFacts/FunFacts.js
@@ -2,19 +2,38 @@ import React from "react";
 import "./FunFacts.css";
 
 function FunFacts() {
-    return (
-        <section className="col-11 bg-light rounded shadow-lg mx-auto my-4 p-4">
-          <p><strong>Fun Facts:</strong></p>
-          <ul>
-            <li>Cat and dog person (why choose a side?), but has neither right now unfortunately</li>
-            <li>Loves planning trips almost as much as taking them.</li>
-            <li>Enjoys Microsoft Excel perhaps a bit too much.</li>
-            <li>Neutral about pineapple on pizza.</li>
-            <li>Recent shower thought: Is a single piece of spaghetti called a spaghetto?</li>
-            <li>Shower thought follow-up: I Googled it, and it in fact is.  Two semesters of Italian paying off right there.</li>
-          </ul>
-        </section>
-    );
+  return (
+    <section className="col-11 bg-light rounded shadow-lg mx-auto my-4 p-4">
+      <p><strong>Fun Facts:</strong></p>
+      <div>
+        <p>
+          Cat and dog person (why choose a side?), but has neither right now
+          unfortunately
+        </p>
+      </div>
+      <div>
+        <p>Loves planning trips almost as much as taking them.</p>
+      </div>
+      <div>
+        <p>Enjoys Microsoft Excel perhaps a bit too much.</p>
+      </div>
+      <div>
+        <p>Neutral about pineapple on pizza.</p>
+      </div>
+      <div>
+        <p>
+          Recent shower thought: Is a single piece of spaghetti called a
+          spaghetto?
+        </p>
+      </div>
+      <div>
+        <p>
+          Shower thought follow-up: I Googled it, and it in fact is. Two
+          semesters of Italian paying off right there.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default FunFacts;
