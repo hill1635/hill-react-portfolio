1dcb019708fec5a51de0fab6b779d186bd6d57a1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 16:25:14 -0700

    Background component imported.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BasicInfo from "./BasicInfo";
+import Background from "./Background";
 import FunFacts from "./FunFacts";
 import "./style.css";
 
@@ -11,40 +12,7 @@ function About() {
           <h1 className="typedText bg-light">Tucker Hill</h1>
         </div>
         <BasicInfo/>
-        <section className="col-11 bg-light rounded shadow-lg mx-auto my-4 p-4">
-          <p><strong>Background: </strong> MERN stack (mostly)</p>
-          {/* Carousel? */}
-          <ul>
-            <li>
-              A lot of my training focused around building web apps using
-              the React JavaScript library.  After creating many walls of 
-              text with vanilla JavaScript, React was a breath of fresh air.
-              The component system and folder structure made conceptual sense
-              and my brain was happy to process code broken up into smaller parts.
-              On top of that, the library helps create very speedy apps that you
-              can edit on the fly, rather than having to re-load the whole page!
-            </li>
-            <li>
-              MongoDB was used primarily as the database to store information for 
-              projects, assignments, and exercises.  The initial set up is a little
-              confusing, but once it is up and rolling, I found MongoDB, in
-              conjunction with the Mongoose OOP library, a lot more intuitive and
-              user-friendly compared to MySQL and Sequelize.
-            </li>
-            <li>
-              To connect apps to databases, Express was almost omnipresent 
-              in all projects, assignments, and exercises.  As with MongoDB and
-              Mongoose, the set up is a little confusing, but is fairly cut-and-paste
-              (literally and figuratively) after that.
-              {/* More Here. */}
-            </li>
-            <li>
-              And finally the engine that powers the car - Node.js.  Not going to lie,
-              it's not the most exciting and shiny object in the stack.
-              {/* More Here. */}
-            </li>
-          </ul>
-        </section>
+        <Background/>
         <FunFacts/>
       </div>
     </main>
