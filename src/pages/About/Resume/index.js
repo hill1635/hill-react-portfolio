5aae44b62b12d43cc324c1cb7679fca3d082074b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 16:30:00 -0700

    Basic structure set.

--- src/pages/About/Resume/index.js
+++ src/pages/About/Resume/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./style.css";
 
-function Resume () {
+function Resume() {
+  return (
+    <section className="col-5 bg-light rounded shadow-lg mx-auto p-4">
+      <h3>For more information...</h3>
+      <p>check out my resume.</p>
+    </section>
+  );
 }
 
 export default Resume;
