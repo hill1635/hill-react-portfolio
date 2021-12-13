496d80f01cb134d083fffd97463c7fa5fa011a85
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 15:47:10 -0700

    Styling and sizing.

--- src/pages/About/FunFacts/FunFacts.js
+++ src/pages/About/FunFacts/FunFacts.js
@@ -3,44 +3,46 @@ import "./FunFacts.css";
 
 function FunFacts() {
   return (
-    <section className="funFacts text-center col-11 col-sm-7 bg-light rounded shadow-lg mx-auto my-4 p-4">
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-        <h3>Fun Facts:</h3>
-        <div className="carousel-inner p-4">
-            <div className="carousel-item active">
+    <section className="funFacts text-center col-11 col-sm-7 bg-light rounded shadow-lg mx-auto p-0 my-4">
+      <div id="carouselExampleIndicators" className="carousel slide h-100 py-4" data-bs-ride="carousel">
+        <header className="header">
+            <h3>Fun Facts:</h3>
+        </header>
+        <div className="carousel-inner facts px-5">
+            <div className="carousel-item active p-5">
             <p>
                 Cat and dog person (why choose a side?), but has neither right now
                 unfortunately
             </p>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item p-5">
             <p>Loves planning trips almost as much as taking them.</p>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item p-5">
             <p>Enjoys Microsoft Excel perhaps a bit too much.</p>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item p-5">
             <p>Neutral about pineapple on pizza.</p>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item p-5">
             <p>
                 Recent shower thought: Is a single piece of spaghetti called a
                 spaghetto?
             </p>
             </div>
-            <div className="carousel-item">
+            <div className="carousel-item p-5">
             <p>
                 Shower thought follow-up: I Googled it, and it in fact is. Two
                 semesters of Italian paying off right there.
             </p>
             </div>
-        </div>
-        <button className="carousel-control-prev bg-transparent text-body h-100" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <button className="directionals carousel-control-prev bg-transparent text-body h-100" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
             <i class="fas fa-chevron-left"></i>
         </button>
-        <button className="carousel-control-next bg-transparent text-body h-100" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <button className="directionals carousel-control-next bg-transparent text-body h-100" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
             <i class="fas fa-chevron-right"></i>
         </button>
+        </div>
       </div>
     </section>
   );
