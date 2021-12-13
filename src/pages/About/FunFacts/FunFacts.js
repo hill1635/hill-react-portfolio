0760491ab3818777c66ecba075c059f5eed9d7cd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 15:53:16 -0700

    More styling.

--- src/pages/About/FunFacts/FunFacts.js
+++ src/pages/About/FunFacts/FunFacts.js
@@ -5,32 +5,33 @@ function FunFacts() {
   return (
     <section className="funFacts text-center col-11 col-sm-7 bg-light rounded shadow-lg mx-auto p-0 my-4">
       <div id="carouselExampleIndicators" className="carousel slide h-100 py-4" data-bs-ride="carousel">
-        <header className="header">
+        <header className="header w-75">
             <h3>Fun Facts:</h3>
+            <hr></hr>
         </header>
         <div className="carousel-inner facts px-5">
-            <div className="carousel-item active p-5">
+            <div className="carousel-item active fact">
             <p>
                 Cat and dog person (why choose a side?), but has neither right now
                 unfortunately
             </p>
             </div>
-            <div className="carousel-item p-5">
+            <div className="carousel-item fact">
             <p>Loves planning trips almost as much as taking them.</p>
             </div>
-            <div className="carousel-item p-5">
+            <div className="carousel-item fact">
             <p>Enjoys Microsoft Excel perhaps a bit too much.</p>
             </div>
-            <div className="carousel-item p-5">
+            <div className="carousel-item fact">
             <p>Neutral about pineapple on pizza.</p>
             </div>
-            <div className="carousel-item p-5">
+            <div className="carousel-item fact">
             <p>
                 Recent shower thought: Is a single piece of spaghetti called a
                 spaghetto?
             </p>
             </div>
-            <div className="carousel-item p-5">
+            <div className="carousel-item fact">
             <p>
                 Shower thought follow-up: I Googled it, and it in fact is. Two
                 semesters of Italian paying off right there.
