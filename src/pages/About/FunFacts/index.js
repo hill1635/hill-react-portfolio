fadf6d7ac33e327360d21b7a70094500400c0514
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 16:37:49 -0700

    More sizing.

--- src/pages/About/FunFacts/index.js
+++ src/pages/About/FunFacts/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function FunFacts() {
   return (
-    <section className="funFacts text-center col-11 col-sm-5 bg-light rounded shadow-lg mx-auto p-0 my-4">
+    <section className="funFacts text-center col-11 col-sm-6 bg-light rounded shadow-lg mx-auto p-0 my-4">
       <div id="carouselExampleIndicators" className="carousel slide h-100 py-4" data-bs-ride="carousel">
         <header className="header w-75">
             <h3>Fun Facts:</h3>
--- src/pages/About/Resume/index.js
+++ src/pages/About/Resume/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Resume() {
   return (
-    <section className="col-5 bg-light rounded shadow-lg mx-auto p-4 my-4">
+    <section className="col-11 col-sm-4 bg-light rounded shadow-lg mx-auto p-4 my-4">
       <h3>For more information...</h3>
       <p>check out my resume.</p>
     </section>
