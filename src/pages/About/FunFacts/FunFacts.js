9d2a73e0767ff40273edc8b4b06b4450cefe05fb
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 12:01:43 -0700

    Moved initial code from page to component.

--- src/pages/About/FunFacts/FunFacts.js
+++ src/pages/About/FunFacts/FunFacts.js
@@ -3,7 +3,18 @@ import "./FunFacts.css";
 
 function FunFacts() {
     return (
-
+        <section className="col-11 bg-light rounded shadow-lg mx-auto my-4 p-4">
+          <p><strong>Fun Facts:</strong></p>
+          {/* Carousel? */}
+          <ul>
+            <li>Cat and dog person (why choose a side?), but has neither right now unfortunately</li>
+            <li>Loves planning trips almost as much as taking them.</li>
+            <li>Enjoys Microsoft Excel perhaps a bit too much.</li>
+            <li>Neutral about pineapple on pizza.</li>
+            <li>Recent shower thought: Is a single piece of spaghetti called a spaghetto?</li>
+            <li>Shower thought follow-up: I Googled it, and it in fact is.  Two semesters of Italian paying off right there.</li>
+          </ul>
+        </section>
     );
 }
 
