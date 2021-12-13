4c30eef4a3c654e0c27d971ec3f81a80353bf7dd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 12:23:57 -0700

    Previous and next buttons added with functionality.

--- src/pages/About/FunFacts/FunFacts.js
+++ src/pages/About/FunFacts/FunFacts.js
@@ -35,6 +35,12 @@ function FunFacts() {
             </p>
             </div>
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
       </div>
     </section>
   );
