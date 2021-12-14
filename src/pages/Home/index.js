b8dfbbc160a15f695467c10b57459f1938b7c598
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-13 18:21:55 -0700

    Condensed code and minimized use of bootstrap classes so it's easier to view.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,30 +3,26 @@ import "./style.css";
 
 function Home() {
   return (
-    <main className="bg-intro col-xs-12 col-md-10 col-lg-8 mt-5 p-0 mx-auto rounded text-center">
-      <div className="header bg-light p-3 mb-5 shadow-lg rounded">
+    <main className="bg-main col-11 col-md-10 col-lg-8 text-center">
+      <header className="header bg-light shadow-lg">
         <h1 className="typedText bg-light">'Hello World!'</h1>
-      </div>
+      </header>
       <div className="clearfix">
-        <div className="biopic bg-light d-inline-block col-6 float-left p-0">
+        <div className="biopic col-6">
           <img
             src="./images/me.jpeg"
             className="rounded-circle shadow-lg m-0 w-100"
           ></img>
         </div>
-        <div className="intro bg-light d-inline-block shadow-lg col-6 float-right mt-5 rounded">
-          <p className="py-5 px-4">
-            My name is Tucker and I'm a full-stack developer.
-          </p>
+        <div className="intro bg-light shadow-lg col-6">
+          <p>My name is Tucker and I'm a full-stack developer.</p>
         </div>
       </div>
-      <div className="learnMore intro bg-light w-25 p-1 rounded">
-        <a href="/about">
-          <span className="next">
-            <p className="mx-auto">Learn more.</p>
-          </span>
-        </a>
-      </div>
+      <a href="/about">
+        <span className="learnMore bg-light shadow-lg">
+          <p>Learn more.</p>
+        </span>
+      </a>
     </main>
   );
 }
