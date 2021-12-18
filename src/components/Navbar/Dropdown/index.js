36491777050168cc7457b325c831ed339214107b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-18 16:54:45 -0700

    Dropdown styling. Installed Sass.

--- src/components/Navbar/Dropdown/index.js
+++ src/components/Navbar/Dropdown/index.js
@@ -4,28 +4,32 @@ import "./style.css";
 function Dropdown() {
   return (
     <div className="dropdown-menu dropdown-menu-right border border-dark" aria-labelledby="dropdownMenuButton1">
-      <ul className="list-group list-group-horizontal list-unstyled">
+      <ul className="list-unstyled">
         <li>
-          <a target="_blank" href="https://www.facebook.com/tucker.hill.75/">
-            <img src="./icons/facebook.svg" className="icon-fb rounded"></img>
+          <a className="contact-link" target="_blank" href="https://www.facebook.com/tucker.hill.75/">
+            <img src="./icons/facebook.svg" className="dropdown-icon-fb rounded"></img>
+            Facebook
           </a>
         </li>
         <li>
-          <a
+          <a className="contact-link"
             target="_blank"
             href="https://www.linkedin.com/in/tucker-hill-5254b979/"
           >
-            <img src="./icons/linkedin.svg" className="icon rounded"></img>
+            <img src="./icons/linkedin.svg" className="dropdown-icon rounded"></img>
+            LinkedIn
           </a>
         </li>
         <li>
-          <a target="_blank" href="mailto:hill1635@gmail.com">
-            <img src="./icons/gmail.svg" className="icon rounded"></img>
+          <a className="contact-link" target="_blank" href="mailto:hill1635@gmail.com">
+            <img src="./icons/gmail.svg" className="dropdown-icon rounded"></img>
+            Gmail
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://www.github.com/hill1635">
-            <img src="./icons/github.svg" className="icon rounded"></img>
+          <a className="contact-link" target="_blank" href="https://www.github.com/hill1635">
+            <img src="./icons/github.svg" className="dropdown-icon rounded"></img>
+            GitHub
           </a>
         </li>
       </ul>
