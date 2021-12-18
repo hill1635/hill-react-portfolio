add1c27c35150fd265b6b1dbd2396999f6cd2b40
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-18 13:54:21 -0700

    Styling.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,19 +8,19 @@ function Home() {
         <h1 className="typedText bg-light">'Hello World!'</h1>
       </header>
       <div className="clearfix">
-        <div className="biopic col-6">
+        <div className="biopic col-md-6 col-sm-12">
           <img
             src="./images/me.jpeg"
             className="rounded-circle shadow-lg m-0 w-100"
           ></img>
         </div>
-        <div className="intro bg-light shadow-lg col-6">
+        <div className="intro bg-light shadow-lg col-md-6 col-sm-12">
           <p>My name is Tucker and I'm a full-stack developer.</p>
         </div>
       </div>
       <a href="/about">
         <span className="learnMore bg-light shadow-lg">
-          <p>Learn more.</p>
+          <p>&gt;&gt;Learn more.&lt;&lt;</p>
         </span>
       </a>
     </main>
