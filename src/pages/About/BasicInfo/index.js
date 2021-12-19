1f53f6c13a8e909b1790a5f45108d04395bbe26b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-19 13:09:20 -0700

    Basic Info styling.

--- src/pages/About/BasicInfo/index.js
+++ src/pages/About/BasicInfo/index.js
@@ -3,44 +3,49 @@ import "./style.scss";
 
 function BasicInfo() {
   return (
-    <section className="basicInfo infoBox col-11 clearfix mx-auto my-4 p-0">
+    <section className="basicInfo infoBox col-11 clearfix mx-auto my-4 p-4">
+      <span className="d-inline-flex">
+        <h3 className="object">Basic Info</h3>
+        <h3 className="character ml-3">&#123;</h3>
+      </span>
       <div className="picDiv col-7 col-md-6 col-lg-5 float-right p-0">
         <img
           src="./images/me.jpeg"
           className="profilePic float-right m-0 w-100"
         ></img>
       </div>
-      <div className="text-wrap p-4">
+      <div className="text-wrap px-4 py-2">
         <p>
-          <strong>From: </strong> Norway, ME
+          <strong className="string">From: </strong> Norway, ME
         </p>
         <p>
-          <strong>Current: </strong> Cottonwood Heights, UT
+          <strong className="string">Current: </strong> Cottonwood Heights, UT
         </p>
         <p>
-          <strong>Nickname: </strong> Tuck
+          <strong className="string">Nickname: </strong> Tuck
         </p>
         <p>
-          <strong>Aliases: </strong>Trevor, Tyler, Hunter, Trucker (it's easy to
+          <strong className="string">Aliases: </strong>Trevor, Tyler, Hunter, Trucker (it's easy to
           mix up my name, apparently)
         </p>
         <p>
-          <strong>Hobbies: </strong>Hiking, Skiing, Traveling, Playing Guitar,
+          <strong className="string">Hobbies: </strong>Hiking, Skiing, Traveling, Playing Guitar,
           Video Games, Cooking (when I feel like it)
         </p>
         <p>
-          <strong>Education: </strong> B.A. in Psychology and Sociology,
+          <strong className="string">Education: </strong> B.A. in Psychology and Sociology,
           University of Vermont
         </p>
         <p>
-          <strong>Certifications: </strong>Full-Stack Development, University of
+          <strong className="string">Certifications: </strong>Full-Stack Development, University of
           Utah Coding Bootcamp
         </p>
         <p>
-          <strong>Languages: </strong>JavaScript, HTML, French (un petit peu),
+          <strong className="string">Languages: </strong>JavaScript, HTML, French (un petit peu),
           Italian (two semesters in college), Sarcasm
         </p>
       </div>
+      <h3 className="character">&#125;</h3>
     </section>
   );
 }
