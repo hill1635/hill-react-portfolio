631001450d0d62ae90735c8b38d7ff59969a69ea
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-12-18 22:10:43 -0700

    Homepage styling rough draft.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,24 +3,35 @@ import "./style.scss";
 
 function Home() {
   return (
-    <main className="bg-main col-11 col-md-10 col-lg-8 text-center">
-      <header className="header shadow-lg">
-        <h1 className="typedText">'Hello World!'</h1>
+    <main className="bg-main col-10 col-md-10 col-lg-9 text-center">
+      <header className="header">
+        <span className="d-inline-flex">
+          <h1 className="header1">&lt;h1&gt;</h1>
+          <h1 className="typedText">'Hello World!'</h1>
+          <h1 className="header1">&lt;/h1&gt;</h1>
+        </span>
       </header>
       <div className="clearfix">
         <div className="biopic col-md-6 col-sm-12">
           <img
             src="./images/me.jpeg"
-            className="rounded-circle shadow-lg m-0 w-100"
+            className="me m-0 w-100"
           ></img>
         </div>
-        <div className="intro bg-light shadow-lg col-md-6 col-sm-12">
-          <p>My name is Tucker and I'm a full-stack developer.</p>
+        <div className="infoBox intro col-md-6 col-sm-12 px-4 py-3">
+          <span className="d-block element m-0">&lt;p&gt;</span>
+          <div className="ml-4">
+            <span>My name is </span>
+            <span className="string">Tucker </span>
+            <span>and I'm a </span>
+            <span className="statement">full-stack developer.</span>
+          </div>
+          <span className="d-block element">&lt;/p&gt;</span>
         </div>
       </div>
       <a href="/about">
-        <span className="learnMore bg-light shadow-lg">
-          <p>&gt;&gt;Learn more.&lt;&lt;</p>
+        <span className="infoBox learnMore p-4 mt-5">
+          <p className="m-0">&lt;LearnMore/&gt;</p>
         </span>
       </a>
     </main>
